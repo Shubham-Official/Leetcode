f98863523bd539afdd906cbf8369843227517e1b
GITHUB-MOFO <s9359713018@gmail.com>
2022-08-08 18:56:29 +0530

    Longest Well-Performing Interval

--- Stack/Medium/Longest Well-Performing Interval(vip).cpp
+++ Stack/Medium/Longest Well-Performing Interval(vip).cpp
@@ -1,36 +1,36 @@
 //Link: https://leetcode.com/problems/longest-well-performing-interval/
 
-//Approach 1: Using Stack
+Approach 1: Using Stack
 
 class Solution {
 public:
     int longestWPI(vector<int>& hours) {
         int n = hours.size();
-        
+
         vector<int> pre(n+1, 0);
         for(int i=0; i<n; ++i) pre[i+1] = pre[i] + (hours[i] > 8 ? 1 : -1);
-        
+
         vector<int> st;
-        
+
         for(int i=0; i<=n; ++i){
             if(st.empty() or pre[st.back()] > pre[i]){
                 st.push_back(i);
             }
         }
-        
+
         int ans = 0;
-        
+
         for(int i = n; i >= 0; --i){
             if(st.empty()) break;
-            
+
             while(!st.empty() and pre[st.back()] < pre[i]){
                 ans = max(ans, i - st.back());
                 st.pop_back();
             }
-            
+
             if(!st.empty() and st.back() == i) st.pop_back();
         }
-        
+
         return ans;
     }
 };
