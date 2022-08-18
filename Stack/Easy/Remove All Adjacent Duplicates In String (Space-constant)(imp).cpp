e4c6bc5d67d6a4978396ad75f08ce62540ee1ad5
GITHUB-MOFO <s9359713018@gmail.com>
2022-08-18 19:01:53 +0530

    Remove All Adjacent Duplicates In String

---
+++ Stack/Easy/Remove All Adjacent Duplicates In String (Space-constant)(imp).cpp
@@ -0,0 +1,16 @@
+//Link: https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        int i = 0, n = s.length();
+        for (int j = 0; j < n; j++, i++) {
+            s[i] = s[j];
+            if (i > 0 && s[i - 1] == s[i]) // count = 2
+                i -= 2;
+        }
+        return s.substr(0, i);
+    }
+};
+
+Time: O(N), Space: O(1).
