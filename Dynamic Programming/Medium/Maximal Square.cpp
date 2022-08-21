61bd1af311c8d675e08a46ed37c9d62191ad61b7
GITHUB-MOFO <s9359713018@gmail.com>
2022-08-21 12:10:02 +0530

    Maximal Square

---
+++ Dynamic Programming/Medium/Maximal Square.cpp
@@ -0,0 +1,34 @@
+//Link: https://leetcode.com/problems/maximal-square/
+
+class Solution {
+public:
+    int dp[301][301];
+    int helperMethod(int i,int j,vector<vector<char>>& matrix)
+    {
+        if(i>= matrix.size()|| j>=matrix[0].size()|| matrix[i][j] == '0')
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        
+        return dp[i][j] = 1+min( helperMethod(i+1,j,matrix),
+                            min(helperMethod(i+1,j+1,matrix),helperMethod(i,j+1,matrix))) ;
+    }
+    
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int ans = INT_MIN;
+        memset(dp,-1,sizeof(dp));
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(matrix[i][j]=='1')
+                {
+                    ans=max(ans,helperMethod(i,j,matrix));
+                }
+            }
+        }
+        return ans==INT_MIN?0:ans*ans;
+    }
+};
